d03e61a9da57000118dc2ca8f25c96af1570a3d8
nhb1998 <849990475@qq.com>
2021-12-09 19:22:29 +0800

    熟悉服务与JSON

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ const http = require('http');   //导入node.js模块
 
 // 创建server
 const server = http.createServer((request, response) => {
-    response.write('hello ~');
+    const data = {
+        id: 1,
+        title: 'JSON',
+        content: 'javascript object notation'
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    response.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+
+    response.write(jsonData);
     response.end();
 });
 
