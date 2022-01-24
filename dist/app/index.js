eb9d4001fe5579cb936eba81a5e885b681356738
nhb1998 <849990475@qq.com>
2022-01-24 20:00:25 +0800

    创建头像模块

--- dist/app/index.js
+++ dist/app/index.js
@@ -10,10 +10,11 @@ const auth_router_1 = __importDefault(require("../auth/auth.router"));
 const file_router_1 = __importDefault(require("../file/file.router"));
 const tag_router_1 = __importDefault(require("../tag/tag.router"));
 const comment_router_1 = __importDefault(require("../comment/comment.router"));
+const avatar_router_1 = __importDefault(require("../avatar/avatar.router"));
 const app_middleware_1 = require("./app.middleware");
 const app = express_1.default();
 app.use(express_1.default.json());
-app.use(post_router_1.default, user_router_1.default, auth_router_1.default, file_router_1.default, tag_router_1.default, comment_router_1.default);
+app.use(post_router_1.default, user_router_1.default, auth_router_1.default, file_router_1.default, tag_router_1.default, comment_router_1.default, avatar_router_1.default);
 app.use(app_middleware_1.defaultErrorHandler);
 exports.default = app;
 //# sourceMappingURL=index.js.map
--- src/app/index.ts
+++ src/app/index.ts
@@ -5,6 +5,7 @@ import authRouter from '../auth/auth.router';
 import fileRouter from '../file/file.router';
 import tagRouter from '../tag/tag.router';
 import commentRouter from '../comment/comment.router';
+import avatarRouter from '../avatar/avatar.router';
 import { defaultErrorHandler } from './app.middleware';
 
 /**
@@ -20,7 +21,7 @@ app.use(express.json());
 /**
  * 路由
  */
-app.use(postRouter, userRouter, authRouter, fileRouter, tagRouter, commentRouter);
+app.use(postRouter, userRouter, authRouter, fileRouter, tagRouter, commentRouter, avatarRouter);
 
 /**
  * 使用默认异常处理器
