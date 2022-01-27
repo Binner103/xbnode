00fcd80f42305d101304b772768b54e5752999d0
nhb1998 <849990475@qq.com>
2022-01-27 19:59:11 +0800

    定义保存点赞内容的功能

--- dist/like/like.service.d.ts
+++ dist/like/like.service.d.ts
@@ -1,1 +1,1 @@
-export {};
+export declare const createUserLikePost: (userId: number, postId: number) => Promise<import("mysql2/typings/mysql/lib/protocol/packets/RowDataPacket")[] | import("mysql2/typings/mysql/lib/protocol/packets/RowDataPacket")[][] | import("mysql2/typings/mysql/lib/protocol/packets/OkPacket") | import("mysql2/typings/mysql/lib/protocol/packets/OkPacket")[] | import("mysql2/typings/mysql/lib/protocol/packets/ResultSetHeader")>;
--- dist/like/like.service.js
+++ dist/like/like.service.js
@@ -1,3 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mysql_1 = require("../app/database/mysql");
+exports.createUserLikePost = async (userId, postId) => {
+    const statement = `
+        INSERT INTO
+            user_like_post (userId, postId)
+        VALUES (?, ?)
+    `;
+    const [data] = await mysql_1.connection.promise().query(statement, [userId, postId]);
+    return data;
+};
 //# sourceMappingURL=like.service.js.map
