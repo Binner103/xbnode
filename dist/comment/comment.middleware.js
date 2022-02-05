738b3147eedb819a92815a59e4ab90ae813c117a
nhb1998 <849990475@qq.com>
2022-02-05 16:28:09 +0800

    获取指定内容的评论列表和获取指定用户的评论列表

--- dist/comment/comment.middleware.js
+++ dist/comment/comment.middleware.js
@@ -1,10 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.filter = async (request, response, next) => {
+    const { post, user, action } = request.query;
     request.filter = {
         name: 'default',
         sql: 'comment.parentId IS NULL',
     };
+    if (post && !user && !action) {
+        request.filter = {
+            name: 'postComments',
+            sql: 'comment.parentId IS NULL AND comment.postId = ?',
+            param: post
+        };
+    }
+    ;
+    if (user && action == 'published' && !post) {
+        request.filter = {
+            name: 'userPublished',
+            sql: 'comment.parentId IS NULL AND comment.userId = ?',
+            param: user
+        };
+    }
     next();
 };
 //# sourceMappingURL=comment.middleware.js.map
--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -8,12 +8,32 @@ export const filter = async (
     response: Response,
     next: NextFunction
 ) => {
+    // 解构查询符
+    const { post, user, action } = request.query;
+
     // 默认的过滤
     request.filter = {
         name: 'default',
         sql: 'comment.parentId IS NULL',
     };
-    
+
+    // 内容的评论
+    if (post && !user && !action) {
+        request.filter = {
+            name: 'postComments',
+            sql: 'comment.parentId IS NULL AND comment.postId = ?',
+            param: post
+        }
+    };
+
+    // 用户的评论
+    if (user && action == 'published' && !post) {
+        request.filter = {
+            name: 'userPublished',
+            sql: 'comment.parentId IS NULL AND comment.userId = ?',
+            param: user
+        }
+    }
     //下一步
     next();
 };
