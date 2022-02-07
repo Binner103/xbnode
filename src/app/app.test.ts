e55fd2acbf8f3940fa6be934db6b83de0d2858df
nhb1998 <849990475@qq.com>
2022-02-07 23:02:41 +0800

    创建接口测试

--- src/app/app.test.ts
+++ src/app/app.test.ts
@@ -1,3 +1,6 @@
+import request from 'supertest';
+import app from '../app';
+import { connection } from './database/mysql';
 import { greet } from "./playground/demo";
 
 /**
@@ -11,5 +14,36 @@ describe('演示单元测试', () => {
         
         // 断言
         expect(greeting).toBe('你好, 农宏彬');
+    });
+});
+
+/**
+ * 测试接口
+ */
+describe('演示接口测试', () => {
+    //测试结束以后断开数据库服务
+    afterAll(async () => {
+        // 断开数据服务连接
+        connection.end();
+    });
+
+    test('测试 GET /', async () => {
+        // 请求接口
+        const response = await request(app).get('/');
+
+        // 断言
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({title:'开发之路'});
+    });
+
+    test('测试 POST /echo', async () => {
+        // 请求接口
+        const response = await request(app)
+            .post('/echo')
+            .send({message: '你好~'});
+
+        // 断言
+        expect(response.status).toBe(201);
+        expect(response.body).toEqual({message: '你好~'});
     });
 });
