d836d9b81fb8d1aca58d150e9d67a562574035b6
nhb1998 <849990475@qq.com>
2022-02-18 16:31:02 +0800

    处理跨域请求错误 cors

--- dist/app/index.js
+++ dist/app/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const post_router_1 = __importDefault(require("../post/post.router"));
 const user_router_1 = __importDefault(require("../user/user.router"));
 const auth_router_1 = __importDefault(require("../auth/auth.router"));
@@ -15,6 +16,10 @@ const like_router_1 = __importDefault(require("../like/like.router"));
 const app_router_1 = __importDefault(require("./app.router"));
 const app_middleware_1 = require("./app.middleware");
 const app = express_1.default();
+app.use(cors_1.default({
+    origin: "*",
+    exposedHeaders: "X-Total-Count",
+}));
 app.use(express_1.default.json());
 app.use(post_router_1.default, user_router_1.default, auth_router_1.default, file_router_1.default, tag_router_1.default, comment_router_1.default, avatar_router_1.default, like_router_1.default, app_router_1.default);
 app.use(app_middleware_1.defaultErrorHandler);
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,30 @@
-import express from 'express';
-import postRouter from '../post/post.router';
-import userRouter from '../user/user.router';
-import authRouter from '../auth/auth.router';
-import fileRouter from '../file/file.router';
-import tagRouter from '../tag/tag.router';
-import commentRouter from '../comment/comment.router';
-import avatarRouter from '../avatar/avatar.router';
-import likeRouter from '../like/like.router';
-import appRouter from './app.router';
-import { defaultErrorHandler } from './app.middleware';
+import express from "express";
+import cors from "cors";
+import postRouter from "../post/post.router";
+import userRouter from "../user/user.router";
+import authRouter from "../auth/auth.router";
+import fileRouter from "../file/file.router";
+import tagRouter from "../tag/tag.router";
+import commentRouter from "../comment/comment.router";
+import avatarRouter from "../avatar/avatar.router";
+import likeRouter from "../like/like.router";
+import appRouter from "./app.router";
+import { defaultErrorHandler } from "./app.middleware";
 
 /**
  * 创建应用
  */
 const app = express();
+
+/**
+ * 跨域资源共享
+ */
+app.use(
+  cors({
+    origin: "*",
+    exposedHeaders: "X-Total-Count",
+  })
+);
 
 /**
  * 处理JSON
@@ -23,7 +34,17 @@ app.use(express.json());
 /**
  * 路由
  */
-app.use(postRouter, userRouter, authRouter, fileRouter, tagRouter, commentRouter, avatarRouter, likeRouter, appRouter);
+app.use(
+  postRouter,
+  userRouter,
+  authRouter,
+  fileRouter,
+  tagRouter,
+  commentRouter,
+  avatarRouter,
+  likeRouter,
+  appRouter
+);
 
 /**
  * 使用默认异常处理器
